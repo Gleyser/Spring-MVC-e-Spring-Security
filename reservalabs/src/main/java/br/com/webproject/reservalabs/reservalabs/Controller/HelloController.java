9eff5b47026fc15aff2fccc7433a84d90697d33b
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2021-04-14 05:40:58 -0300

    exemplo simples com Thymeleaf

--- reservalabs/src/main/java/br/com/webproject/reservalabs/reservalabs/Controller/HelloController.java
+++ reservalabs/src/main/java/br/com/webproject/reservalabs/reservalabs/Controller/HelloController.java
@@ -1,6 +1,7 @@
 package br.com.webproject.reservalabs.reservalabs.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.Mapping;
 public class HelloController {
 	
 	@GetMapping("/hello")
-	public String hello() {
+	public String hello(Model model) {
+		model.addAttribute("nome", "Maria José");
 		return "hello";
 	}
 
